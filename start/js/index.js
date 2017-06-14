289f3f022fc78fa32a3d9a4e976e3e5e507982ca
A1rPun <thombakker@gmail.com>
2017-06-14 16:40:38 +0200

    :rocket: Add start-up project

---
+++ start/js/index.js
@@ -0,0 +1,12 @@
+var noteTaking = new Vue({
+  el: '#NoteTaking',
+  created: function () {
+    // "Constructor" logic
+  },
+  data: {
+    // Static view data
+  },
+  methods: {
+    // View logic (events)
+  },
+});
