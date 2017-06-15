19b22f80d855a1e9229da362dae9dcdda5cf4265
Jos van Stekelenborg <josvanstekelenborg@gmail.com>
2017-06-15 14:01:29 +0200

    test

--- start/js/index.js
+++ start/js/index.js
@@ -5,8 +5,41 @@ var noteTaking = new Vue({
   },
   data: {
     // Static view data
+      notes:
+      ['capicity'],
   },
   methods: {
     // View logic (events)
+      notePrompt: function () {
+          var note = prompt('Put words towards your idea', ' ');
+          if (note) {
+              this.notes.push(note);
+          }
+      },
+      deleteNote: function (index) {
+          var message = "Verwijder de 'note'\n" + this.notes[index]; 
+          if (confirm(message)) {
+              this.notes.splice(index, 1);
+          }
+      },
+
+      getNotes: function () {
+          var notes;
+          try {
+              var value = localStorage.getItem(noteKey);
+              notes = JSON.parse(value) || [];
+          } catch (e) {
+              notes = [];
+          }
+          return notes;
+      },
+      setNotes: function () {
+          var value = JSON.stringify(this.notes);
+          localStorage.setItem(noteKey, value);
+      },
   },
+  created: function () {
+      this.notes = this.getNotes();
+  },
+        
 });
